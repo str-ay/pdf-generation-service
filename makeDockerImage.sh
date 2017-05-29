9e92e760fea48caa42821d411e425ce1046eaccd
Aleksandr Streltsov <jness.pro@gmail.com>
2017-05-29 15:17:43 +0300

    build fix

--- makeDockerImage.sh
+++ makeDockerImage.sh
@@ -1,6 +1,10 @@
 docker kill pdfgs
 docker rm pdfgs
-./gradlew -Pprofile=docker clean makeDockerExecutable &&
+ver="$(cat build.gradle | grep "def ver = " | tr -d "'")" &&
+ver="$(echo ${ver:10})" &&
+
+./gradlew -Pprofile=docker clean bootRepackage &&
+cp build/libs/pdf-generation-service-${ver}.war docker/pdf-generation-service.war
 cd docker &&
 docker build -t jnesspro/pdf-generation-service .
 #docker run --name pdfgs -d -p 8085:8080 jnesspro/pdf-generation-service
