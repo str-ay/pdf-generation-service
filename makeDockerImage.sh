23a649771ab62160fa0c0abe9054cb6401b65e31
Aleksandr Streltsov <jness.pro@gmail.com>
2017-05-29 15:09:29 +0300

    build fix

--- makeDockerImage.sh
+++ makeDockerImage.sh
@@ -3,5 +3,5 @@ docker rm pdfgs
 ./gradlew -Pprofile=docker clean makeDockerExecutable &&
 cd docker &&
 docker build -t jnesspro/pdf-generation-service .
-#docker run --name pdfgs -d -p 80:8080 jnesspro/pdf-generation-service
+#docker run --name pdfgs -d -p 8085:8080 jnesspro/pdf-generation-service
 #docker exec pdfgs fc-list
